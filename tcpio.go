c36de25bdc7663d65af24eac65a5566c6dcd04b0
Jack Morris <jack.morris10@gmail.com>
2016-03-27 13:25:26 +0100

    Added code

---
+++ tcpio.go
@@ -0,0 +1,42 @@
+// tcpio - simple program to connect to a remote tcp socket and send/receieve data between
+// the remote port and stdin/out.
+// Exits on any failure (eg. connection closed, either side closed, ...)
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	// Get the remote host.
+	if len(os.Args) != 2 {
+		log.Fatal("Usage: netcat \"<ip>:<port>\"")
+	}
+
+	// Connect to the remote host.
+	conn, err := net.Dial("tcp", os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Copy data both ways.
+	done := make(chan struct{})
+	go passData(os.Stdout, conn, done) // conn -> stdout
+	go passData(conn, os.Stdin, done)  // stdin -> conn
+
+	// Exit when either data pass is done.
+	<-done
+	conn.Close()
+}
+
+func passData(dst io.Writer, src io.Reader, done chan<- struct{}) {
+	// Pass data from src to dst, and print log on error.
+	// Signal on `done` when complete (whether or not from error).
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Print(err)
+	}
+	done <- struct{}{}
+}
